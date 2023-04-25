57b11b24c38742324345063aaa518426a2ef2174
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-04-25 17:29:40 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(document.getElementById('guessed-number').value)
+
+  // process
+  if (numberGuessed == randomNumber) {
+
+  // output
+  document.getElementById('answer').innerHTML = numberGuessed
+}
