f3c438101d95c16801fd591bce5875f3cbec563b
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-02 13:31:58 +0000

    finished extra

--- go_app/main.go
+++ go_app/main.go
@@ -1,36 +1,51 @@
 // Created by: Dominic M.
 // Created on: April 2023
 //
-// This program converts fahrenheit to celsius.
+// This program is a game where you guess a number from 1-6 and try to match the computer's number.
 package main
 
 
 import (
   "fmt"
+  "math/rand"
+  "time"
 )
 
 
 func main() {
-  var celsius float64
-  var fahrenheit float64
+  var input int
 
 
   // input
-  fmt.Println("This program converts fahrenheit to celsius.")
+  fmt.Println("This program is a game where you guess a number from 1-6 and try to match the computer's number.")
   fmt.Println()
-  fmt.Print("Enter a temperature in fahrenheit: ")
-  fmt.Scanln(&fahrenheit)
+  fmt.Print("Please enter a number: ")
+  fmt.Scanln(&input)
 
 
   // process
-  celsius = (fahrenheit - 32) * 5 / 9
+  s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+  random := r1.Intn(6) + 1
+
+//  This program is an infinite loop until you guess the correct number.
+//  for input != random {
+//    fmt.Println("You guessed incorrectly.")
+//    fmt.Println()
+//    fmt.Print("Please enter a number: ")
+//    fmt.Scanln(&input)
+//  }
+
+//   fmt.Println("You guessed correctly!")
 
 
-  // output
-  fmt.Println()
-  celsiusFormatted := fmt.Sprintf("%.2f", celsius)
-  fmt.Println("The temperature is:", celsiusFormatted, "°C")
-
+  // This program is a loop that will only run once.
+  if input == random {
+    fmt.Println("You guessed correctly!")
+  } 
+  if input != random {
+    fmt.Println("You guessed incorrectly.")
+  }
 
   fmt.Println("\nDone.")
 }
